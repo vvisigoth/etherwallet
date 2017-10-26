99bcfb9ef5c5f758176b38600081e86246b213fc
Anthony <anthony@ai.co>
2017-10-25 17:05:58 -0700

    service to pull in templates

---
+++ app/scripts/services/templateService.js
@@ -0,0 +1,10 @@
+'use strict';
+const state = require('../../partial/state.html');
+const test2 = require('../../partial/test2.html');
+
+var templateService = {
+    state: state,
+    t2: test2
+};
+
+module.exports = templateService;
