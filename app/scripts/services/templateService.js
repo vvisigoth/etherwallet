41f5a0755d3eb3c8b013e55f83cb8027da8b2f55
Anthony <anthony@ai.co>
2017-10-26 19:27:11 -0700

    add liquidate to partials

--- app/scripts/services/templateService.js
+++ app/scripts/services/templateService.js
@@ -1,10 +1,12 @@
 'use strict';
 const state = require('../../partial/state.html');
 const test2 = require('../../partial/test2.html');
+const liquidate = require('../../partial/liquidate.html');
 
 var templateService = {
     state: state,
-    t2: test2
+    t2: test2,
+    liquidate: liquidate
 };
 
 module.exports = templateService;
