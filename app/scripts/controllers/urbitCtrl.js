ec213c0ccd6f7173aea5054dd906ce9576199acb
Anthony <anthony@ai.co>
2017-12-18 15:17:08 -0800

    put in locktime counter

--- app/scripts/controllers/urbitCtrl.js
+++ app/scripts/controllers/urbitCtrl.js
@@ -451,6 +451,63 @@ var urbitCtrl = function($scope, $sce, $routeParams, $location, $rootScope, wall
     //
     //UI Conviences
     //
+    $scope.secToString = function(secs) {
+      var min = 60;
+      var hour = 60 * min;
+      var day = 24 * hour;
+      var week = 7 * day;
+      var year = 52 * week;
+      var fy = function(s) {
+        if (s < year) {
+          return ['', s];
+        } else {
+          return [Math.round(s / year) + 'y', s % year];
+        }
+      }
+      var fw = function(tup) {
+        var str = tup[0];
+        var sec = tup[1];
+        if (sec < week) {
+          return [str, sec];
+        } else {
+          return [str + ' ' + Math.round(sec / week) + 'w', sec % week];
+        }
+      }
+      var fd = function(tup) {
+        var str = tup[0];
+        var sec = tup[1];
+        if (sec < day) {
+          return [str, sec];
+        } else {
+          return [str + ' ' + Math.round(sec / day) + 'd', sec % day];
+        }
+      }
+      var fh = function(tup) {
+        var str = tup[0];
+        var sec = tup[1];
+        if (sec < hour) {
+          return [str, sec];
+        } else {
+          return [str + ' ' + Math.round(sec / hour) + 'h', sec % hour];
+        }
+      }
+      var fm = function(tup) {
+        var str = tup[0];
+        var sec = tup[1];
+        if (sec < min) {
+          return [str, sec];
+        } else {
+          return [str + ' ' + Math.round(sec / min) + 'm', sec % min];
+        }
+      }
+      var fs = function(tup) {
+        var str = tup[0];
+        var sec = tup[1];
+        return str + ' ' + sec + 's';
+      }
+      return fs(fm(fh(fd(fw(fy(secs))))));
+    }
+
     $scope.formatShipName = function(ship) {
       if (!ship) {
         return ship;
@@ -744,6 +801,7 @@ var urbitCtrl = function($scope, $sce, $routeParams, $location, $rootScope, wall
       });
       function put(data) {
         $scope.ownedShips[ship]['state'] = data[1];
+        $scope.ownedShips[ship]['locktime'] = data[2];
       }
     }
     $scope.readOwnedShips = function(addr) {
